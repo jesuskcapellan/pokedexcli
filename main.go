e5f39255e0f8ab384e8b4176ef8310ff39189c3f
Jesus Capellan <jesus.k.capellan@outlook.com>
2026-05-24 14:53:00 -0600

    add repl loop

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		if !scanner.Scan() {
+			fmt.Printf("%s\n", scanner.Err())
+		}
+		input := scanner.Text()
+		cleanedInput := cleanInput(input)
+		fmt.Printf("Your command was: %s\n", cleanedInput[0])
+	}
 }
